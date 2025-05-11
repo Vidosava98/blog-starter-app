b0849f7c0ad3fd0cbdcf91679b1a5302504fbe76
Vidosava <arsic.vida@gmail.com>
2025-05-11 20:33:19 +0200

    Add change.

--- src/app/_components/theme-switcher.tsx
+++ src/app/_components/theme-switcher.tsx
@@ -19,36 +19,36 @@ export const NoFOUCScript = (storageKey: string) => {
   /* can not use outside constants or function as this script will be injected in a different context */
   const [SYSTEM, DARK, LIGHT] = ["system", "dark", "light"];
 
-  /** Modify transition globally to avoid patched transitions */
-  const modifyTransition = () => {
-    const css = document.createElement("style");
-    css.textContent = "*,*:after,*:before{transition:none !important;}";
-    document.head.appendChild(css);
-
-    return () => {
-      /* Force restyle */
-      getComputedStyle(document.body);
-      /* Wait for next tick before removing */
-      setTimeout(() => document.head.removeChild(css), 1);
-    };
-  };
-
-  const media = matchMedia(`(prefers-color-scheme: ${DARK})`);
-
   /** function to add remove dark class */
-  window.updateDOM = () => {
-    const restoreTransitions = modifyTransition();
-    const mode = localStorage.getItem(storageKey) ?? SYSTEM;
-    const systemMode = media.matches ? DARK : LIGHT;
-    const resolvedMode = mode === SYSTEM ? systemMode : mode;
-    const classList = document.documentElement.classList;
-    if (resolvedMode === DARK) classList.add(DARK);
-    else classList.remove(DARK);
-    document.documentElement.setAttribute("data-mode", mode);
-    restoreTransitions();
-  };
-  window.updateDOM();
-  media.addEventListener("change", window.updateDOM);
+  if (typeof document !== "undefined") {
+    /** Modify transition globally to avoid patched transitions */
+    const modifyTransition = () => {
+      const css = document.createElement("style");
+      css.textContent = "*,*:after,*:before{transition:none !important;}";
+      document.head.appendChild(css);
+      return () => {
+        /* Force restyle */
+        getComputedStyle(document.body);
+        /* Wait for next tick before removing */
+        setTimeout(() => document.head.removeChild(css), 1);
+      };
+    };
+    const media = matchMedia(`(prefers-color-scheme: ${DARK})`);
+
+    window.updateDOM = () => {
+      const restoreTransitions = modifyTransition();
+      const mode = localStorage.getItem(storageKey) ?? SYSTEM;
+      const systemMode = media.matches ? DARK : LIGHT;
+      const resolvedMode = mode === SYSTEM ? systemMode : mode;
+      const classList = document.documentElement.classList;
+      if (resolvedMode === DARK) classList.add(DARK);
+      else classList.remove(DARK);
+      document.documentElement.setAttribute("data-mode", mode);
+      restoreTransitions();
+    };
+    window.updateDOM();
+    media.addEventListener("change", window.updateDOM);
+  }
 };
 
 let updateDOM: () => void;
@@ -61,7 +61,7 @@ const Switch = () => {
     () =>
       ((typeof localStorage !== "undefined" &&
         localStorage.getItem(STORAGE_KEY)) ??
-        "system") as ColorSchemePreference,
+        "system") as ColorSchemePreference
   );
 
   useEffect(() => {
