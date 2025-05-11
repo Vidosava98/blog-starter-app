ca693fb9cd21748dfedf7b51e23723213ecd4be5
Vidosava <arsic.vida@gmail.com>
2025-05-11 13:15:02 +0200

    Fix layout.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,13 +1,12 @@
 import Footer from "@/app/_components/footer";
 import { CMS_NAME, HOME_OG_IMAGE_URL } from "@/lib/constants";
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
 import cn from "classnames";
 import { ThemeSwitcher } from "./_components/theme-switcher";
 import Header from "./_components/header";
 import "./globals.css";
 
-const inter = Inter({ subsets: ["latin"] });
+//const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: `Vida's portfolio`,
@@ -58,7 +57,7 @@ export default function RootLayout({
         <link rel="alternate" type="application/rss+xml" href="/feed.xml" />
       </head>
       <body
-        className={cn(inter.className, "dark:bg-slate-900 dark:text-slate-400")}
+        className={cn("dark:bg-slate-900 dark:text-slate-400")}
         suppressHydrationWarning
       >
         <ThemeSwitcher />
